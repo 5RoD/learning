0e3438df43018fea4b5cdccea7ac73f1a1fa4cb8
5RoD <mohamad.fallaha@student.rocva.nl>
2025-10-03 10:14:40 +0200

    Controller maken met Artisan

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlanetController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,55 +11,5 @@ Route::get('/planeten', function () {
 	return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
 });
 
-Route::get('/planets', function () {
-	$planets = [
-		[
-			'name' => 'Mars',
-			'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-		],
-		[
-			'name' => 'Venus',
-			'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-		],
-		[
-			'name' => 'Earth',
-			'description' => "Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things."
-		],
-		[
-			'name' => 'Jupiter',
-			'description' => "Jupiter is a gas giant and doesn't have a solid surface, but it may have a solid inner core about the size of Earth."
-		],
-	];
-
-	return view('planets', ['planets' => $planets]);
-});
-
-Route::get('/planets/{planet}', function ($planet) {
-	$planets = [
-		[
-			'name' => 'Mars',
-			'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-		],
-		[
-			'name' => 'Venus',
-			'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-		],
-		[
-			'name' => 'Earth',
-			'description' => "Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things."
-		],
-		[
-			'name' => 'Jupiter',
-			'description' => "Jupiter is a gas giant and doesn't have a solid surface, but it may have a solid inner core about the size of Earth."
-		],
-	];
-
-	$lookup = ucfirst(strtolower($planet));
-	$foundPlanet = collect($planets)->where('name', $lookup)->first();
-
-	if (!$foundPlanet) {
-		abort(404, 'Planet not found');
-	}
-
-	return view('planet-details', ['planet' => $foundPlanet]);
-});
+Route::get('/planets', [PlanetController::class, 'index']);
+Route::get('/planets/{planet}', [PlanetController::class, 'show']);
